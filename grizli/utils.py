3521c9a887f8ab46eef18591ea3bba74893fc56e
TheSkyentist <raphael.hviding@gmail.com>
2024-11-08 11:08:31 +0100

    Adding back in first_uniqid to properly keep track

--- grizli/utils.py
+++ grizli/utils.py
@@ -8282,6 +8282,7 @@ def drizzle_from_visit(
                 calc_wcsmap=calc_wcsmap,
                 verbose=verbose,
                 data=data,
+                first_uniqid=count + 1,
             )
 
             outsci, outwht, outvar, outctx = res[:4]
@@ -8353,6 +8354,7 @@ def drizzle_array_groups(
     calc_wcsmap=False,
     verbose=True,
     data=None,
+    first_uniqid=1,
 ):
     """
     Drizzle array data with associated wcs
@@ -8400,6 +8402,9 @@ def drizzle_array_groups(
         ``data = outsci, outwht, outctx, varnum``, where ``varnum = outvar * outwht``
 
         If not provided, new arrays will be created.
+
+    first_uniqid : int, optional
+        First `uniqid` value to use for the drizzle for contex maps
 
     Returns
     -------
@@ -8566,7 +8571,7 @@ def drizzle_array_groups(
             "cps",
             1,
             wcslin_pscale=wcs_list[i].pscale,
-            uniqid=i+1,
+            uniqid=first_uniqid + i,
             pixfrac=pixfrac,
             kernel=kernel,
             fillval="0",
@@ -8586,7 +8591,7 @@ def drizzle_array_groups(
                 "cps",
                 1,
                 wcslin_pscale=wcs_list[i].pscale,
-                uniqid=i+1,
+                uniqid=first_uniqid + i,
                 pixfrac=pixfrac,
                 kernel=kernel,
                 fillval="0",
